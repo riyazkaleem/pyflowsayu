23372954487c3b9853e6120ad0b9fbb36b2e1af4
riyaz kaleem <riyaz.marx@gmail.com>
2024-05-12 10:34:31 -0700

    modified the code a bit.

--- d7payu.py
+++ d7payu.py
@@ -15,14 +15,29 @@ listOfWords = ['my', 'name', 'is', 'riyaz', 'kaleem']
 selectedWord = random.choice(listOfWords)
 print(f'selectedWord: {selectedWord}')
 
+#converting the selected word into a list
+selectedWordList = splitFunction(selectedWord)
+print(f'selected word list: {selectedWordList}')
+
+#creating a blank string with same characters as selected word
+selectedWordLength = len(selectedWord)
+updatedList=[]
+for i in range(selectedWordLength):
+    updatedList.append('_')
+print(f'updated list: {updatedList}')
+
 #take letter inputs from the user and check if the letter is in the chosen word
 inputLetter = input('guess a letter u think is in the word:\n').lower()
 
 #checking for letter in word and updating the list with each letter position compared
-selectedWordList = splitFunction(selectedWord)
-print(f'selected word list: {selectedWordList}')
+listCounter = 0
 for letter in selectedWordList:
     if (inputLetter == letter):
         print(f'match')
+        updatedList[listCounter]=letter
+        listCounter+=1
+        print(f'updatedList: {updatedList}')
     else:
         print(f'no match')
+        listCounter+=1
+        print(f'updatedList: {updatedList}')
