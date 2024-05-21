42f5c760a24fe8fb1940af5598e1f9ef2e63df2f
riyaz kaleem <riyaz.marx@gmail.com>
2024-05-20 21:23:43 -0700

    updating hangman code: 21 23 may 20 2024. implementing ascii art for hangman.

--- d7payu.py
+++ d7payu.py
@@ -8,65 +8,14 @@ def splitFunction(inputword):
     for character in inputword:
         splitList.append(character)
     return splitList
-
-#function to check if the letter is in the word
-# def letterCheckFunction(inputLetter, selectedWord):
-#     if inputLetter in selectedWord:
-#         return True
-#     else:
-#         return False
-    
-#function to prepare list of appearence of checked character
-# def updatingCorrectList(inputLetter, selectedWord, updatedList):
-#     for index in range(len(selectedWord)):
-#         if inputLetter == selectedWord[index]:
-#             updatedList[index] = inputLetter
-#         else:
-#             continue
-#     return updatedList
-
-#function to check if all the characters are filled
-# def checkForUnderscores(finalPrintList):
-#     if '_' in finalPrintList:
-#         return True
-#     else:
-#         return False
     
 #function to replace all instances of matching letter
 def replaceAllLetterMatch(inputLetter, updatedList):
-    #game of replacing correct underscores
-    counter = 0
-
-    # for letter in updatedList:
-    #     #check if all characters are filled
-    #     # underscoreCheckFlag = checkForUnderscores(finalPrintList)
-    #     # if underscoreCheckFlag == True:
-    #     #     #assigning the correct word length to the correct length counter
-    #     #     correctLengthCounter = selectedWordLength
-    #     #     #checking the letter is in the word
-    #     #     letterInWordFlag = letterCheckFunction(inputLetter, selectedWord)
-    #     #     #printing the checked letter
-    #     #     finalPrintList = updatingCorrectList(inputLetter, selectedWord, updatedList) 
-
-    #     #logic function implementation for replacing matched letter with word and others with an underscore
-    #     if counter < len(updatedList):
-    #         if letter == inputLetter:
-    #             counter += 1 
-    #             # print(updatedList)
-    #             continue 
-    #         else:
-    #             updatedList[counter] = '_'
-    #             counter += 1
-    #             # print(updatedList)
-
     #check if the letter in the list
     if inputLetter in updatedList:
         return True
     else:
-        return False
-
-    #returning final updated list
-    return updatedList       
+        return False      
 
 #function to implement the actual game logic
 def actualGameLogic(updatedList):
@@ -79,7 +28,7 @@ def actualGameLogic(updatedList):
         #take letter inputs from the user 
         inputLetter = input('guess a letter u think is in the word:\n').lower()
 
-        print(f'updated list before implementing letter replace function {updatedList}')
+        #print(f'updated list before implementing letter replace function {updatedList}')
         #implementation of the letter replace function
         if replaceAllLetterMatch(inputLetter, updatedList):
             lettersMatching.append(inputLetter)
@@ -94,7 +43,8 @@ def actualGameLogic(updatedList):
 def matchingLetter(lettersMatching, updatedList):
     print(f'updated list inside matching letter function: {updatedList}')
     print(f'letters matching inside matching letter function: {lettersMatching}')
-    updatedCombinedList = []
+    # updatedCombinedList = []
+    deathCounter = 6
     for letter in updatedList:
         print(f'letter: {letter}')
         if letter in lettersMatching:
@@ -102,8 +52,10 @@ def matchingLetter(lettersMatching, updatedList):
         else:
             updatedtempstring = letterReplace(letter, updatedList) 
             updatedList = splitFunction(updatedtempstring)
-            print(f'updatedtempstring: {updatedtempstring}')
-            print(f'updatedlist: {updatedList}')
+            #print(f'updatedtempstring: {updatedtempstring}')
+            print(f'lose one attempt and the updated list is: {updatedList}')
+            print(stages[deathCounter])
+            deathCounter -= 1
 
     print(f'updated list inside matching letter function: {updatedList}')
     return updatedList
@@ -122,6 +74,64 @@ def gameWonCheck(updatedCombinedList):
         return True
 
 #implementation
+#declaring ascii art for hangman
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 #from a group of words selecting a word
 listOfWords = ['my', 'name', 'is', 'riyaz', 'kaleem']
 selectedWord = random.choice(listOfWords)
@@ -137,7 +147,7 @@ updatedList=[]
 for letter in selectedWordList:
     updatedList.append(letter)
 # finalPrintList=updatedList
-print(f'updated list: {updatedList}')
+print(f'list we are working on: {updatedList}')
 
 #implementation of actual game logic
 lettersMatching = []
@@ -153,14 +163,3 @@ if gameWonFLag == True:
     print('You won')
 else:
     print('You lose')
-
-# print(f'updated list: {updatedCombinedList}')
-
-#check if the person lost or won
-# if "_" in updatedCombinedList:
-#     print("You Lose")
-# else:
-#     print("You Win")
-
-# #final test print statement
-# print(f'updated list: {updatedList}')
