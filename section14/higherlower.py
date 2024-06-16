7549c16dab0dbdb0d95b66e2654c27f41c227536
riyaz kaleem <riyaz.marx@gmail.com>
2024-06-15 19:38:56 -0700

    final code update higher lower game 🥲

--- section14/higherlower.py
+++ section14/higherlower.py
@@ -15,16 +15,23 @@ winflag = True
 scorecount = 0
 # print(f'index1: {randomindex1}, index2: {randomindex2}')
 # print(f'data1: {gamedata.data[randomindex1][name]}, data1: {gamedata.data[randomindex1][name]}')
-print(art.logo)
+# print(art.logo)
 randomindex1 = random.randint(0, gamedatacount-1)
 randomindex2 = random.randint(0, gamedatacount-1)
-print(f'compare A: {gamedata.data[randomindex1][name]}, {gamedata.data[randomindex1][description]} from {gamedata.data[randomindex1][country]}')
-print(art.vs)
-print(f'compare B: {gamedata.data[randomindex2][name]}, {gamedata.data[randomindex2][description]} from {gamedata.data[randomindex2][country]}')
-while(winflag): 
+# print(f'compare A: {gamedata.data[randomindex1][name]}, {gamedata.data[randomindex1][description]} from {gamedata.data[randomindex1][country]}')
+# print(art.vs)
+# print(f'compare B: {gamedata.data[randomindex2][name]}, {gamedata.data[randomindex2][description]} from {gamedata.data[randomindex2][country]}')
+# userinput = input("Who has more followers? Type 'A' or 'B':")
+while(winflag):
+    os.system('cls') 
+    print(art.logo)
+    print(f"You're Current score: {scorecount}.")
+    print(f'compare A: {gamedata.data[randomindex1][name]}, {gamedata.data[randomindex1][description]} from {gamedata.data[randomindex1][country]}')
+    print(art.vs)
+    print(f'compare B: {gamedata.data[randomindex2][name]}, {gamedata.data[randomindex2][description]} from {gamedata.data[randomindex2][country]}')
     userinput = input("Who has more followers? Type 'A' or 'B':")
     if userinput == 'A':
-        if gamedata.data[randomindex1][followers] > gamedata.data[randomindex2][followers]:
+        if gamedata.data[randomindex1][followers] >= gamedata.data[randomindex2][followers]:
             scorecount+=1
             os.system('cls')
             print(art.logo)
@@ -39,7 +46,7 @@ while(winflag):
             print(f"Sorry, that's wrong. Final score: {scorecount}")
             winflag = False
     elif userinput == 'B':
-        if gamedata.data[randomindex2][followers] > gamedata.data[randomindex1][followers]:
+        if gamedata.data[randomindex2][followers] >= gamedata.data[randomindex1][followers]:
             scorecount+=1
             os.system('cls')
             print(art.logo)
@@ -49,6 +56,7 @@ while(winflag):
             print(f'compare B: {gamedata.data[randomindex2][name]}, {gamedata.data[randomindex2][description]} from {gamedata.data[randomindex2][country]}')
             randomindex1 = randomindex2
             randomindex2 = random.randint(0, gamedatacount-1)
+            # userinput = input("Who has more followers? Type 'A' or 'B':")
         else:
             print(art.logo)
             print(f"Sorry, that's wrong. Final score: {scorecount}")
